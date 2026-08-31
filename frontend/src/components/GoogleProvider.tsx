e794038fa0abc6b3e951d0a1f05a96e3a9faa42b
Harshal Patel <harshalkr220@gmail.com>
2026-09-01 03:31:42 +0530

    m

--- frontend/src/components/GoogleProvider.tsx
+++ frontend/src/components/GoogleProvider.tsx
@@ -2,8 +2,10 @@
 
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
+const DEFAULT_CLIENT_ID = "544083718174-gbhrgj904413l9dt83n236p1301udl08.apps.googleusercontent.com";
+
 export default function GoogleProvider({ children }: { children: React.ReactNode }) {
-  const clientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || "";
+  const clientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || DEFAULT_CLIENT_ID;
 
   return (
     <GoogleOAuthProvider clientId={clientId}>
